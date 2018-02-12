a2a1e2a47954c1aa691870964a68644bd08f9d8c
Bas Bloemsaat <bas@bloemsaat.com>
2018-02-12 21:24:47 +0100

    parse excel

--- ds_python/bin/parse_xlsx.py
+++ ds_python/bin/parse_xlsx.py
@@ -2,7 +2,10 @@
 
 import getopt
 import inspect
+import json
+import ntpath
 import os
+import re
 import sys
 import time
 
@@ -14,9 +17,10 @@ import dateutil.parser
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
 
-print(os.path.dirname(__file__))
+# print(os.path.dirname(__file__))
 
 import dartsense
+
 
 def main(argv):
     parameters = {
@@ -38,20 +42,21 @@ def main(argv):
         elif opt in ("-f", "--file"):
             parameters['filename'] = arg
 
-    pprint(parameters)
+    # pprint(parameters)
 
     if not parameters['filename']:
-        usage()        
+        usage()
+
+    sheet_data = []
 
     wb = openpyxl.load_workbook(parameters['filename'], read_only=True)
     # pprint(wb)
 
     for sheet in wb.worksheets:
         # pprint(sheet)
-        print(sheet.title)
+        # print(sheet.title)
         date_avond = dateutil.parser.parse(sheet.title)
-        pprint(date_avond)
-        
+        # pprint(date_avond)
 
         header = [cell.value for cell in sheet[1]]
 
@@ -59,13 +64,21 @@ def main(argv):
         iter_rows = sheet.rows
         next(iter_rows)
         for row in iter_rows:
-            values = {}
+            values = {
+                "Date": date_avond.strftime('%Y-%m  -%d')
+            }
             for key, cell in zip(header, row):
-                pass
                 values[key] = cell.value
-        
+                sheet_data.append(values)
 
-
+    if(sheet_data):
+        filename = re.sub(r'xlsx$',r'json', parameters['filename'])
+        if (filename != parameters['filename']):
+            filename = 'var/www/' + filename
+            pprint(filename)
+            with open(filename, 'w') as outfile:
+                # pass
+                json.dump(sheet_data, outfile)
 
 def usage():
     print('''read_file.py 
