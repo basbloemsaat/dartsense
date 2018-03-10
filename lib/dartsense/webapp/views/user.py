d057edb320c819524a9bb862cdbd50dcd9b97ac7
Bas Bloemsaat <bas@bloemsaat.com>
2018-03-10 20:18:36 +0100

    google login

--- lib/dartsense/webapp/views/user.py
+++ lib/dartsense/webapp/views/user.py
@@ -5,28 +5,26 @@ from flask_oauthlib.client import OAuth
 
 from pprint import pprint
 
+
 oauth = OAuth(app)
 oauth_apps = {}
 for key in config.oauth2:
     oauth_apps[key] = oauth.remote_app(
         key,
-        consumer_key=config.oauth2[key]['consumer_key'],
-        consumer_secret=config.oauth2[key]['consumer_secret'],
-        request_token_params=config.oauth2[key]['request_token_params'],
-        base_url=config.oauth2[key]['base_url'],
-        request_token_url=config.oauth2[key]['request_token_url'],
-        access_token_method=config.oauth2[key]['access_token_method'],
-        access_token_url=config.oauth2[key]['access_token_url'],
-        authorize_url=config.oauth2[key]['authorize_url'],
+        **config.oauth2[key]
     )
+
+google = oauth_apps['google']
+
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
 
 
 @app.route('/user/')
 @app.route('/user')
 def user_index():
-    session['test'] = 'bla2'
-    session['test2'] = 'bla3'
-
     return render_template('user.j2html')
 
 
@@ -44,14 +42,13 @@ def authorized():
     resp = oauth_apps[session['current_provider']].authorized_response()
     pprint(session['current_provider'])
     pprint(resp)
-    # resp = oauth_apps[session['current_provider']].authorized_response()
-    # if resp is None:
-    #     return 'Access denied: reason=%s error=%s' % (
-    #         request.args['error_reason'],
-    #         request.args['error_description']
-    #     )
-    # session['google_token'] = (resp['access_token'], '')
 
-    return ""
-    # me = oauth_apps[session['current_provider']].get('userinfo')
-    # return jsonify({"data": me.data})
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session[session['current_provider'] + '_token'] = (resp['access_token'], '')
+
+    me = oauth_apps[session['current_provider']].get('userinfo')
+    return jsonify({"data": me.data})
