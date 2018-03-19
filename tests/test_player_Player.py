ad336dcd3ce3fd826758520a4ced49762d71ab8d
Bas Bloemsaat <bas@bloemsaat.com>
2018-03-19 08:20:26 +0100

    some extra tests

--- tests/test_player_Player.py
+++ tests/test_player_Player.py
@@ -8,9 +8,10 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
 from dartsense.player import Player
+import dartsense.competition
 
 
-def test_player_init():
+def test_player_init(setup_db):
     player = Player()
     assert isinstance(player, Player)
 
@@ -22,7 +23,19 @@ def test_player_init():
     assert player.name == 'test player 1'
 
 
-def test_player_load():
+def test_player_load(setup_db):
     player = Player(id=pytest.setup_vars['player1_id'])
     assert isinstance(player, Player)
     assert player.name == 'test player 1'
+
+def test_player_Competitions(setup_db):
+    player = Player(id=pytest.setup_vars['player1_id'])
+    assert hasattr(player, 'competitions')
+    competitions = player.competitions
+
+    assert isinstance(competitions, dartsense.competition.CompetitionList)
+    assert len(competitions) == 1
+    assert isinstance(competitions[0],  dartsense.competition.Competition)
+    assert competitions[0].name == 'test league 1'
+
+
