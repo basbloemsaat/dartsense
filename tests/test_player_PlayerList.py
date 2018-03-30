25258883401389fc89332f241a429c83dce4302c
Bas Bloemsaat <bas@bloemsaat.com>
2018-03-30 18:26:52 +0200

    simple search player by name

--- tests/test_player_PlayerList.py
+++ tests/test_player_PlayerList.py
@@ -7,23 +7,27 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
-from dartsense.player import Player
-from dartsense.player import PlayerList
+import dartsense.player
 
 
 player_list = None
 
 
 def test_player_list_init(setup_db):
-    player_list = PlayerList()
+    player_list = dartsense.player.PlayerList()
 
-    assert isinstance(player_list, PlayerList)
+    assert isinstance(player_list, dartsense.player.PlayerList)
     assert len(player_list) == 4
 
     for player in player_list:
-        assert isinstance(player, Player)
+        assert isinstance(player, dartsense.player.Player)
+
 
 def test_player_list_filter(setup_db):
-    player_list = PlayerList(filters={'competition':pytest.setup_vars['testleague1_id']})
+    player_list = dartsense.player.PlayerList(filters={'competition': pytest.setup_vars['testleague1_id']})
     assert len(player_list) == 2
 
+def test_player_list_search(setup_db):
+    player_list = dartsense.player.PlayerList(search='player 3')
+
+    assert len(player_list) == 1
