c5692d2e680c736a80cf44f2c80171841f951c2b
Bas Bloemsaat <bas@bloemsaat.com>
2018-04-07 21:13:22 +0200

    app tests

---
+++ tests/webapp/test_app_user.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../lib/"))
+
+import pytest
+
+from dartsense.webapp import app as tl_app
+tl_app.config['DEBUG'] = True
+tl_app.config['TESTING'] = True
+
+
+@pytest.fixture
+def app():
+    return tl_app
+
+
+def test_user_index(client):
+    res = client.get('/user')
+    assert res.status_code == 302
+
+    res = client.get('/user/')
+    assert res.status_code == 302
+
+
+def test_user_login(client):
+    res = client.get('/user/login')
+    assert res.status_code == 200
+
+    res = client.get('/user/login/')
+    assert res.status_code == 200
+
+
+def test_user_login_provider(client):
+    res = client.get('/user/login/google')
+    assert res.status_code == 302
+
+
+def test_user_logout(client):
+    res = client.get('/user/logout')
+    assert res.status_code == 302
+
+    res = client.get('/user/logout/')
+    assert res.status_code == 302
+
+def test_user_auth(client):
+    # no test for now at least
+    pass
+
