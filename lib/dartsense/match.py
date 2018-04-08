ef95875a306b384e586123a7901e5f4f354b8fe1
Bas Bloemsaat <bas@bloemsaat.com>
2018-04-08 20:04:46 +0200

    from laptop

--- lib/dartsense/match.py
+++ lib/dartsense/match.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import hashlib
 
 from dartsense import db, List_C
+import dartsense.event
 import dartsense.player
 
 
@@ -18,6 +19,19 @@ class Match:
         self._player2_id = None
         self._player1 = None
         self._player2 = None
+
+        self._event_id = None
+        self._event = None
+
+        self.player_1_score = 0
+        self.player_1_180s = 0
+        self.player_1_lollies = 0
+        self.player_1_finishes = []
+
+        self.player_2_score = 0
+        self.player_2_180s = 0
+        self.player_2_lollies = 0
+        self.player_2_finishes = []
 
         if id and not (player1 or player2):
             sql = '''
@@ -40,6 +54,50 @@ class Match:
                 r = res[0]
                 self._player1_id = r['player_1_id']
                 self._player2_id = r['player_2_id']
+                self._event_id = r['event_id']
+
+                self.player_1_score = r['player_1_score']
+                self.player_1_180s = r['player_1_180s']
+                self.player_1_lollies = r['player_1_lollies']
+
+                self.player_2_score = r['player_2_score']
+                self.player_2_180s = r['player_2_180s']
+                self.player_2_lollies = r['player_2_lollies']
+              
+
+    def save(self):
+        if self._id:
+            # update
+            pass
+        else:
+            if not (
+                self._player1_id and
+                self._player2_id
+            ):
+                return False
+            # insert
+            sql = '''
+                INSERT INTO `match`
+                (
+                    `match_id`, `event_id`, `match_date`, 
+                    `match_date_round`, `match_type`,
+                    `player_1_id`, `player_1_id_orig`,
+                    `player_1_score`, `player_1_180s`, `player_1_lollies`,
+                    `player_2_id`, `player_2_id_orig`,
+                    `player_2_score`, `player_2_180s`, `player_2_lollies`
+                )
+                VALUES
+                (
+                    %s, %s, %s, 
+                    %s, %s, 
+                    %s, %s, 
+                    %s, %s, %s, 
+                    %s, %s, %s, 
+                    %s, %s 
+                )
+            '''
+
+        return self._id
 
     def _get_id(self):
         return self._id
@@ -52,58 +110,19 @@ class Match:
     def _get_player_2(self):
         return self._get_player(2)
 
+    def _get_event(self):
+        if not self._event and self._event_id:
+            return dartsense.event.Event(id=self._event_id)
+
+        return None
+
     def _get_player(self, nr):
         player = getattr(self, '_player' + str(nr))
         player_id = getattr(self, '_player' + str(nr) + '_id')
         if not player and player_id:
             player = dartsense.player.Player(id=player_id)
         return player
-        # return getattr(self, '_player' + str(nr) + '_id')
 
     player_1 = property(_get_player_1)
     player_2 = property(_get_player_2)
-
-
-# class PlayerList(List_C):
-
-#     def __init__(self, filters={}, search=""):
-#         List_C.__init__(self)
-#         self._filters = filters
-#         self._searchstr = search
-
-#     def _search(self, force=False):
-#         if force or self._elements == None:
-#             self._elements = []
-#             args = []
-
-#             sql = '''
-#                 SELECT DISTINCT p.*
-#                 FROM player p
-#                     LEFT JOIN competition_player lp ON lp.player_id=p.player_id
-#                 WHERE 1=1
-#             '''
-
-#             if len(self._filters) > 0:
-#                 if 'competition' in self._filters:
-#                     sql += 'AND lp.competition_id=%s '
-#                     args.append(self._filters['competition'])
-
-#             if self._searchstr:
-#                 sql += 'AND p.player_name LIKE %s'
-#                 args.append('%' + self._searchstr + '%')
-
-#             res = db.exec_sql(sql, args)
-
-#             for r in res:
-#                 self._elements.append(Player(
-#                     id=r['player_id'],
-#                     name=r['player_name'],
-#                     nickname=r['player_nickname'],
-#                 ))
-
-#     def add_player(self, player):
-#         self.self._elements[player.id] = player
-
-#     def _get_players(self):
-#         return self.elements
-#     players = property(_get_players)
+    event = property(_get_event)
--- tests/test_event_Match.py
+++ tests/test_event_Match.py
@@ -7,8 +7,9 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
-import dartsense.match 
-import dartsense.player 
+import dartsense.event
+import dartsense.match
+import dartsense.player
 
 
 def test_match_Match_init():
@@ -24,10 +25,34 @@ def test_match_Match_init():
     assert hasattr(match, 'player_1')
     assert match.player_2 == None
 
+    assert hasattr(match, 'event')
+    assert match.event == None
+
+    assert hasattr(match, 'player_1_score')
+    assert match.player_1_score == 0
+    assert hasattr(match, 'player_2_score')
+    assert match.player_2_score == 0
+
+    assert hasattr(match, 'player_1_180s')
+    assert match.player_1_180s == 0
+    assert hasattr(match, 'player_2_180s')
+    assert match.player_2_180s == 0
+
+    assert hasattr(match, 'player_1_lollies')
+    assert match.player_1_lollies == 0
+    assert hasattr(match, 'player_2_lollies')
+    assert match.player_2_lollies == 0
+
+    assert hasattr(match, 'player_1_finishes')
+    assert match.player_1_finishes == []
+    assert hasattr(match, 'player_2_finishes')
+    assert match.player_2_finishes == []
+
+
 def test_match_Match_load(setup_db):
     match = dartsense.match.Match(id=pytest.setup_vars['match1_id'])
     assert isinstance(match, dartsense.match.Match)
-    
+
     assert hasattr(match, 'id')
     assert match.id == pytest.setup_vars['match1_id']
 
@@ -39,8 +64,17 @@ def test_match_Match_load(setup_db):
     assert isinstance(match.player_2, dartsense.player.Player)
     assert match.player_2.id == pytest.setup_vars['player2_id']
 
+    assert isinstance(match.event, dartsense.event.Event)
+    assert match.event.id == pytest.setup_vars['testcompetition1_round1_id']
 
 
+def test_match_Match_save(setup_db):
+    match = dartsense.match.Match()
+    assert match.id == None
+
+    assert hasattr(match, 'save')
+    assert match.save() == False
+    assert match.id == None
 
 
 
