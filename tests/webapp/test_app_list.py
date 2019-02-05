4b46b800189027b109d48bc764faaedc5a02b96e
Bas Bloemsaat <bas@bloemsaat.com>
2019-02-05 18:58:50 +0100

    rudimentary authorization

--- tests/webapp/test_app_list.py
+++ tests/webapp/test_app_list.py
@@ -17,58 +17,100 @@ def app(setup_db):
     return tl_app
 
 
-def test_list_index(client):
-    tl_app.config['TEST_LOGIN'] = False
-    res = client.get('/list')
-    assert res.status_code == 401
-
-    res = client.get('/list/')
-    assert res.status_code == 401
-
+def login(client):
     tl_app.config['TEST_LOGIN'] = True
     res = client.get('/user', follow_redirects=True)
 
     cookie = res.headers['Set-Cookie']
-    match = re.search(r'dartsense_session=([^;]+);',cookie)
+    match = re.search(r'dartsense_session=([^;]+);', cookie)
     sessionid = match.group(1)
 
-    client.set_cookie('localhost', 'dartsense_session',sessionid)
+    client.set_cookie('localhost', 'dartsense_session', sessionid)
+
+
+def test_list_index(client):
+    res = client.get('/list')
+    assert res.status_code == 401
+    res = client.get('/list/')
+    assert res.status_code == 401
+
+    login(client)
+
+    res = client.get('/list')
+    assert res.status_code == 200
     res = client.get('/list/')
     assert res.status_code == 200
-    
 
 
-# def test_list_competitions(client):
-#     res = client.get('/list/competitions')
-#     assert res.status_code == 200
+def test_list_competitions(client):
+    res = client.get('/list/competitions')
+    assert res.status_code == 401
+    res = client.get('/list/competitions/')
+    assert res.status_code == 401
 
-#     res = client.get('/list/competitions/')
-#     assert res.status_code == 200
+    login(client)
 
-
-# def test_list_competition(client):
-#     res = client.get('/list/competition/' +
-#                      str(pytest.setup_vars['testtournament1_id']))
-#     assert res.status_code == 200
-#     res = client.get('/list/competition/99999999')
-#     assert res.status_code == 404
+    res = client.get('/list/competitions')
+    assert res.status_code == 200
+    res = client.get('/list/competitions/')
+    assert res.status_code == 200
 
 
-# def test_list_player(client):
-#     res = client.get('/list/player/' + str(pytest.setup_vars['player1_id']))
-#     assert res.status_code == 200
-#     res = client.get('/list/player/999999999')
-#     assert res.status_code == 404
+def test_list_competition(client):
+    res = client.get('/list/competition/' +
+                     str(pytest.setup_vars['testtournament1_id']))
+    assert res.status_code == 401
+    res = client.get('/list/competition/99999999')
+    assert res.status_code == 401
+
+    login(client)
+
+    res = client.get('/list/competition/' +
+                     str(pytest.setup_vars['testtournament1_id']))
+    assert res.status_code == 200
+    res = client.get('/list/competition/99999999')
+    assert res.status_code == 404
 
 
-# def test_list_organisations(client):
-#     res = client.get('/list/organisations')
-#     assert res.status_code == 200
+def test_list_player(client):
+    res = client.get('/list/player/' + str(pytest.setup_vars['player1_id']))
+    assert res.status_code == 401
+    res = client.get('/list/player/999999999')
+    assert res.status_code == 401
 
-# def test_list_organisation(client):
-#     res = client.get('/list/organisation/' + str(pytest.setup_vars['organisation1_id']))
+    login(client)
 
-# def test_list_event(client):
-#     res = client.get('/list/event/' + str(pytest.setup_vars['testcompetition1_round1_id']))
-#     assert res.status_code == 200
+    res = client.get('/list/player/' + str(pytest.setup_vars['player1_id']))
+    assert res.status_code == 200
+    res = client.get('/list/player/999999999')
+    assert res.status_code == 404
 
+
+def test_list_organisations(client):
+    res = client.get('/list/organisations')
+    assert res.status_code == 401
+
+    login(client)
+
+    res = client.get('/list/organisations')
+    assert res.status_code == 200
+
+
+def test_list_organisation(client):
+    res = client.get('/list/organisation/' + str(pytest.setup_vars['organisation1_id']))
+    assert res.status_code == 401
+
+    login(client)
+
+    res = client.get('/list/organisation/' + str(pytest.setup_vars['organisation1_id']))
+    assert res.status_code == 200
+
+
+def test_list_event(client):
+    res = client.get('/list/event/' + str(pytest.setup_vars['testcompetition1_round1_id']))
+    assert res.status_code == 401
+
+    login(client)
+
+    res = client.get('/list/event/' + str(pytest.setup_vars['testcompetition1_round1_id']))
+    assert res.status_code == 200
