f7fdd37cf328f28ff591ac68891a4990541a0a50
Bas Bloemsaat <bas@bloemsaat.com>
2019-11-14 23:35:34 +0100

    nog wat kleine wijzigingen

--- docs/dartsapp.js
+++ docs/dartsapp.js
@@ -1,84 +1,82 @@
- var dartsapp = {};
- (function() {
-     this.data = [];
-     this.data_indexes = {
-         "bydate": {},
-         "byplayer": {},
-     };
+var dartsapp = {};
+(function() {
+    this.data = [];
+    this.data_indexes = {
+        "bydate": {},
+        "byplayer": {},
+    };
 
-     this.players_name_lookup = {
-         "Andor": "Andor",
-         "Anil": "Anil",
-         "Bas": "Bas",
-         "Bert": "Bert",
-         "Brandon": "Brandon",
-         "Christa": "Christa",
-         "Colin": "Colin",
-         "Ed": "Ed",
-         "Elbert": "Elbert",
-         "Erik H": "Erik H",
-         "Erik": "Erik H",
-         "Ernie": "Ernie",
-         "Frank": "Frank",
-         "Frans": "Frans",
-         "Gert J": "Gert J",
-         "Gert": "Gert J",
-         "Gijs": "Gijs",
-         "Gilbert": "Gilbert",
-         "Hans": "Hans",
-         "Harry": "Harry",
-         "Henri": "Henri",
-         "Jari": "Jari",
-         "Johan": "Johan",
-         "John": "John",
-         "Joost": "Joost",
-         "Joris": "Joris",
-         "JR": "JR",
-         "Kim": "Kim",
-         "Maik": "Maik",
-         "Marianne": "Marianne",
-         "Martin": "Martin",
-         "Menno": "Menno",
-         "Otto": "Otto",
-         "Paplip": "Paplip",
-         "Pietra": "Pietra",
-         "Reyn": "Reyn",
-         "Youri": "Youri"
-     }
+    this.players_name_lookup = {
+        "Andor": "Andor",
+        "Anil": "Anil",
+        "Bas": "Bas",
+        "Bert": "Bert",
+        "Brandon": "Brandon",
+        "Christa": "Christa",
+        "Colin": "Colin",
+        "Ed": "Ed",
+        "Elbert": "Elbert",
+        "Erik H": "Erik H",
+        "Erik": "Erik H",
+        "Ernie": "Ernie",
+        "Frank": "Frank",
+        "Frans": "Frans",
+        "Gert J": "Gert J",
+        "Gert": "Gert J",
+        "Gijs": "Gijs",
+        "Gilbert": "Gilbert",
+        "Hans": "Hans",
+        "Harry": "Harry",
+        "Henri": "Henri",
+        "Jari": "Jari",
+        "Johan": "Johan",
+        "John": "John",
+        "Joost": "Joost",
+        "Joris": "Joris",
+        "JR": "JR",
+        "Kim": "Kim",
+        "Maik": "Maik",
+        "Marianne": "Marianne",
+        "Martin": "Martin",
+        "Menno": "Menno",
+        "Otto": "Otto",
+        "Paplip": "Paplip",
+        "Pietra": "Pietra",
+        "Reyn": "Reyn",
+        "Youri": "Youri"
+    }
 
-     this.add_item_to_index = function(index, key, item) {
-         if (!this.data_indexes[index][key]) {
-             this.data_indexes[index][key] = [];
-         }
-         this.data_indexes[index][key].push(item);
-     }
+    this.add_item_to_index = function(index, key, item) {
+        if (!this.data_indexes[index][key]) {
+            this.data_indexes[index][key] = [];
+        }
+        this.data_indexes[index][key].push(item);
+    }
 
-     this.add_data = function(data) {
-         this.data = this.data.concat(data);
-         // console.log(data);
+    this.add_data = function(filename, data) {
+        this.data = this.data.concat(data);
 
-         // meer manieren om naar de data te kijken: indexen maken.
-         for (let i = 0; i < data.length; i++) {
-             let item = data[i];
-             // console.log(item);
+        let competitie_naam = filename.replace(/\.json$/,'').replace(/_/g,' ');
+        console.log(competitie_naam)
 
-             item['Year'] =item['Date'].substr(0,4);
-             item['Seizoen'] = "?"; // uitzoeken hoe to te voegen
 
-             //d3.nest werkt hier ook, maar niet bij spelers
-             this.add_item_to_index('bydate', item['Date'], item);
+        // meer manieren om naar de data te kijken: indexen maken.
+        for (let i = 0; i < data.length; i++) {
+            let item = data[i];
 
-             if (item['Speler']) {
-                 this.add_item_to_index('byplayer', this.players_name_lookup[item['Speler']], item);
-             } else {
-                 this.add_item_to_index('byplayer', this.players_name_lookup[item['Speler1']], item);
-                 this.add_item_to_index('byplayer', this.players_name_lookup[item['Speler2']], item);
-             }
-         }
+            item['Year'] = item['Date'].substr(0, 4);
+            item['Seizoen'] = competitie_naam; 
 
-         testvar = d3.nest()
-             .key(function(d) { return d.Date; }).object(data);
+            //d3.nest werkt hier ook, maar niet bij spelers en ik loop er toch al door
+            this.add_item_to_index('bydate', item['Date'], item);
 
-     }
+            if (item['Speler']) {
+                this.add_item_to_index('byplayer', this.players_name_lookup[item['Speler']], item);
+            } else {
+                this.add_item_to_index('byplayer', this.players_name_lookup[item['Speler1']], item);
+                this.add_item_to_index('byplayer', this.players_name_lookup[item['Speler2']], item);
+            }
+        }
+    }
 
- }).apply(dartsapp);
+}).apply(dartsapp);
