77952f40efebf3bff517fe49130135a092646d0f
Bas Bloemsaat <bas@bloemsaat.com>
2019-12-21 18:03:52 +0100

    json gen pt 1

---
+++ bin/sva_darts_gen_json.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import getopt
+import re
+import sys
+
+import libsvadarts as sva
+
+from pprint import pprint
+
+def main(argv):
+    # parameters = {
+    #     'filename': '',
+    # }
+
+    # try:
+    #     opts, args = getopt.getopt(
+    #         argv, "hsp:v:",
+    #         [
+    #             "help", "file="])
+    # except getopt.GetoptError as err:
+    #     print(err)
+    #     usage()
+
+    # for opt, arg in opts:
+    #     if opt in ('-h', '--help'):
+    #         usage()
+    #     elif opt in ("-f", "--file"):
+    #         parameters['filename'] = arg
+
+    # if not parameters['filename']:
+    #     usage()
+
+    # pprint(parameters)
+    # data = sva.load_xlsx(parameters['filename'])
+
+    # # pprint(data)
+    # json_filename = re.sub(r'xlsx$', r'json', parameters['filename'])
+    # json_filename = re.sub(r'[^/]+/',r'', json_filename)
+
+    # sva.save_data_to_json(data, json_filename)
+
+
+    # per seizoen
+    competitions = sva.exec_select_query('''
+        SELECT DISTINCT comp
+        FROM game
+        ORDER BY comp
+    ''')
+    for competition in competitions:
+        pprint(competition)
+
+        data = sva.exec_select_query('''
+            SELECT *
+            FROM game
+            WHERE comp=?
+        ''', [competition['comp']])
+
+        pprint(data)
+
+
+    # per speler
+
+
+
+
+def usage():
+    print('''read_file.py 
+        --file <filename>
+        ''')
+    sys.exit()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
